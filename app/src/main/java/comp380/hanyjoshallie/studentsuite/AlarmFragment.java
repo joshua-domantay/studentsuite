85fa30691e5d7305f2c645fd6890d80a86b08807
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2022-03-18 15:55:32 -0700

    Change layout to spinner mode of TimePicker. Test add alarm button for getting time from TimePicker

--- app/src/main/java/comp380/hanyjoshallie/studentsuite/AlarmFragment.java
+++ app/src/main/java/comp380/hanyjoshallie/studentsuite/AlarmFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AlarmFragment extends Fragment {
+    private TimePicker timePicker;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        View _v = inflater.inflate(R.layout.fragment_alarm, container, false);
+        timePicker = _v.findViewById(R.id.alarmTimePicker);
+        setAddAlarmBtn(_v);
+        return _v;
+    }
+
+    private void setAddAlarmBtn(View _v) {
+        Button _addAlarmBtn = _v.findViewById(R.id.addAlarmBtn);
+        _addAlarmBtn.setOnClickListener(item -> {
+            ((TextView) _v.findViewById(R.id.alarmTest)).setText(timePicker.getHour() + " : " + timePicker.getMinute());
+        });
     }
 }
